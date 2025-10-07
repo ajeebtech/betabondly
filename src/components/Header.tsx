f6a1338ac9a31c1ca89bf00a4627d871be6734db
ajeebtech <jatintheboy@gmail.com>
2025-10-08 04:44:10 +0530

    line

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,7 @@ export function Header() {
     return nameOrEmail[0].toUpperCase();
   };
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center -space-x-1 group">
